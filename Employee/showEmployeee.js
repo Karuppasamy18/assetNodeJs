fa0406470a6dded5224557dacdda0d72da0dd484
RK <89678928+Karuppasamy18@users.noreply.github.com>
2024-07-15 14:12:25 +0530

    Add files via upload

---
+++ Employee/showEmployeee.js
@@ -0,0 +1,21 @@
+const sequelize = require('../../db');  // Import the sequelize instance
+const Employee = require('./employee');  // Import the Employee model
+
+(async () => {
+  try {
+    // Ensure the database is synced
+    await sequelize.sync();
+
+    // Fetch all employees
+    const employees = await Employee.findAll();
+
+    console.log('All emplssssoyees:'+employees);
+    employees.forEach(employee => {
+      console.log("sssssssssss"+employee.toJSON());
+    });
+  } catch (err) {
+    console.error('Error fetching employees:', err);
+  } finally {
+    await sequelize.close();   
+  }
+})();
